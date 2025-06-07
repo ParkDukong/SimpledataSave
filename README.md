8f53859e183ab12cbcc114dc4e9cb4e64c1a8ba4
peaisyamyam <jiwan6942@gmail.com>
2025-06-08 07:21:35 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# SimpledataSave
+```
+make by ParkDukong
+```
+
+## datasave(file_name, file_value, file_directory = None, file_type = None)
+```
+datasave("score", 3, "./scores", "txt") # score.txt >> 3
+```
+
+## dataview(file_name)
+```
+dataview("score.txt") # string type 3
+```
+### intdataview(file_name)
+```
+intdataview("score.txt") # int type 3
+```
+
+enjoy!
---
+++ simpledatasave.py
@@ -0,0 +1,27 @@
+
+def datasave(file_name, file_value, file_directory = None, file_type = None):
+    if file_directory == None:
+        if file_type == None:
+            f = open(f"./{file_name}.txt", "w", encoding="utf-8")
+            f.write(str(file_value))
+        else:
+            f = open(f"./{file_name}.{file_type}", "w", encoding="utf-8")
+            f.write(str(file_value))
+    else:
+        if file_type == None:
+            f = open(f"{file_directory}/{file_name}.txt", "w", encoding="utf-8")
+            f.write(str(file_value))
+        else:
+            f = open(f"{file_directory}/{file_name}.{file_type}", "w", encoding="utf-8")
+            
+            f.write(str(file_value))
+
+def dataview(file_name):
+    f = open(f"{file_name}", "r")
+    content = f.read()
+    return content
+
+def intdataview(file_name):
+    f = open(f"{file_name}", "r")
+    content = f.read()
+    return int(content)
