abe934f17c5355bb4e2fada280c718a736af581d
ParkDukong <jiwan6942@gmail.com>
2025-06-21 20:25:29 +0900

    function makes useful

--- simpledatasave.py
+++ simpledatasave.py
@@ -1,27 +1,18 @@
 
-def datasave(file_name, file_value, file_directory = None, file_type = None):
+def datasave(file_name, file_value, file_directory = None):
     if file_directory == None:
-        if file_type == None:
-            f = open(f"./{file_name}.txt", "w", encoding="utf-8")
-            f.write(str(file_value))
-        else:
-            f = open(f"./{file_name}.{file_type}", "w", encoding="utf-8")
-            f.write(str(file_value))
+        f = open(f"./{file_name}", "w", encoding="utf-8")
+        f.write(str(file_value))
     else:
-        if file_type == None:
-            f = open(f"{file_directory}/{file_name}.txt", "w", encoding="utf-8")
-            f.write(str(file_value))
-        else:
-            f = open(f"{file_directory}/{file_name}.{file_type}", "w", encoding="utf-8")
-            
-            f.write(str(file_value))
-
+        f = open(f"{file_directory}/{file_name}.txt", "w", encoding="utf-8")
+        f.write(str(file_value))
+        
 def dataview(file_name):
-    f = open(f"{file_name}", "r")
+    f = open(f"{file_name}", "r", encodeing="utf-8")
     content = f.read()
     return content
 
 def intdataview(file_name):
-    f = open(f"{file_name}", "r")
+    f = open(f"{file_name}", "r", encodeing="utf-8")
     content = f.read()
     return int(content)
